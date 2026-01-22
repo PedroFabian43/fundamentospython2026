da86d2af8c8905a96ac3092933913e63dc64022e
PedroFabian43 <pmfabiangarcia@gmail.com>
2026-01-23 00:07:06 +0100

    He completado la practica

--- python06practica01.py
+++ python06practica01.py
@@ -1,27 +1,76 @@
-dni = int(input("dime tu número de DNI sin la letra: "))
+dni = int(input("dime tu DNI sin la letra: "))
 
-resultado = dni - (int(dni / 23)) * 23
+letradni = dni - (int(dni / 23)) * 23
 
-if (resultado == 0):
-    print("T")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-elif (resultado == 1):
-    print("R")
-print (resultado)
+if (letradni == 0):
+    print("Tu letra es la: T")
+    print("Tu DNI es:", str(dni)+"T")
+elif (letradni == 1):
+    print("Tu letra es la: R")
+    print("Tu DNI es: ", str(dni)+"R")
+elif (letradni == 2):
+    print("Tu letra es la: W")
+    print("Tu DNI es:", str(dni)+"W")
+elif (letradni == 3):
+    print("Tu letra es la: A")
+    print("Tu DNI es:", str(dni)+"A")
+elif (letradni == 4):
+    print("Tu letra es la: G")
+    print("Tu DNI es:", str(dni)+"G")
+elif (letradni == 5):
+    print("Tu letra es la: M")
+    print("Tu DNI es:", str(dni)+"M")
+elif (letradni == 6):
+    print("Tu letra es la: Y")
+    print("Tu DNI es:", str(dni)+"Y")
+elif (letradni == 7):
+    print("Tu letra es la: F")
+    print("Tu DNI es:", str(dni)+"F")
+elif (letradni == 8):
+    print("Tu letra es la: P")
+    print("Tu DNI es:", str(dni)+"P")
+elif (letradni == 9):
+    print("Tu letra es la: D")
+    print("Tu DNI es:", str(dni)+"D")
+elif (letradni == 10):
+    print("Tu letra es la: X")
+    print("Tu DNI es:", str(dni)+"X")
+elif (letradni == 11):
+    print("Tu letra es la: B")
+    print("Tu DNI es:", str(dni)+"B")
+elif (letradni == 12):
+    print("Tu letra es la: N")
+    print("Tu DNI es:", str(dni)+"N")
+elif (letradni == 13):
+    print("Tu letra es la: J")
+    print("Tu DNI es:", str(dni)+"J")
+elif (letradni == 14):
+    print("Tu letra es la: Z")
+    print("Tu DNI es:", str(dni)+"Z")
+elif (letradni == 15):
+    print("Tu letra es la: S")
+    print("Tu DNI es:", str(dni)+"S")
+elif (letradni == 16):
+    print("Tu letra es la: Q")
+    print("Tu DNI es:", str(dni)+"Q")
+elif (letradni == 17):
+    print("Tu letra es la: V")
+    print("Tu DNI es:", str(dni)+"V")
+elif (letradni == 18):
+    print("Tu letra es la: H")
+    print("Tu DNI es:", str(dni)+"H")
+elif (letradni == 19):
+    print("Tu letra es la: L")
+    print("Tu DNI es:", str(dni)+"L")
+elif (letradni == 20):
+    print("Tu letra es la: C")
+    print("Tu DNI es:", str(dni)+"C")
+elif (letradni == 21):
+    print("Tu letra es la: K")
+    print("Tu DNI es:", str(dni)+"K")
+elif (letradni == 22):
+    print("Tu letra es la: E")
+    print("Tu DNI es:", str(dni)+"E")
+else:
+    print("Tu letra es la: T")
+    print("Tu DNI es:", str(dni)+"T")
