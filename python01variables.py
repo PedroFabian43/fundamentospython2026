c1caafec0a2dcb437fea94f9cb975600954eddcf
“PedroFabian43“ <pmfabiangarcia@gmail.com>
2026-01-22 18:54:32 +0100

    first commit

---
+++ python01variables.py
@@ -0,0 +1,13 @@
+#Declarar variables: nos inventamos un nombre y añadimos la variable que es
+#dibujar en la pantalla se hace con variables de tipo String las cuales siempre entre "" son textos literales por eso ""
+#print ("Esto es una variable string")
+# podemos dibujar numeros y esos son las variables tipo Int o Float segun si son numeros enteros o decimales.
+
+nombre = "Paco"
+numero = 24
+
+print(5 + 3)
+print("Su nombre es " + str(numero))
+
+print(7 % 5)
+gygytgcygc
