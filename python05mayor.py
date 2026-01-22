c17e4e0adf12c9c1710b9676c1fe2339effbdbb8
“PedroFabian43“ <pmfabiangarcia@gmail.com>
2026-01-22 20:03:25 +0100

    he hecho cambios en el codigo

--- python05mayor.py
+++ python05mayor.py
@@ -1,16 +1,19 @@
 print("Juguemos a un juego")
 
-print("Dime un número")
-n1 = input()
+#print("Dime un número")
+#n1 = input()
 
-print("Dime otro número")
-n2 = input()
+#print("Dime otro número")
+#n2 = input()
+
+n1 = int(input("Dime un número: "))
+n2 = int(input("Dime otro número: "))
 
 if (n1 > n2):
-    print("El primer numero es mayor")
+    print("El mayor es ", n1)
 elif (n1 == n2):
     print("Ambos números son iguales")
 else:
-    print("El segundo número es mayor")
+    print("El mayor es ", n2)
 
 print("Fin del programa")
