4f8bad4a8232ec3c82082d512c70a3bba78e3d42
“PedroFabian43“ <pmfabiangarcia@gmail.com>
2026-02-09 20:23:07 +0100

    he metido el scropt oracle

--- python34conectaroracle.py
+++ python34conectaroracle.py
@@ -8,10 +8,46 @@ connection = oracledb.connect(user="SYSTEM", password="oracle", dsn="localhost/F
 
 print("estamos conectados!!!")
 
-#las consultas select se ejecutan a partir de connection y se crea un objeto llamado curso
+#las consultas select se ejecutan a partir de connection y se crea un objeto llamado cursor
 
 #cursor = connection.cursor()
 
 #una vez tenemos el cursor se quejecuta el sql.
 
 #cursor.execute(SQL)
+
+#una vez tenemos todo, ya tendremos los datos
+
+#creamos nuestra consulta SQL
+#La consulta SQL desde python no finaliza en ";"
+sql = "select * from DEPT"
+
+#creamos un cursor para la consulta
+cursor = connection.cursor()
+
+#debemos ejecutar la consulta para que nos devuelva los datos de oracle
+cursor.execute(sql)
+
+#aquí ya están los datos
+#una vez que tenemos el cursor,debemos leer los datos
+#tenemos un método llamado FETCHONE() que s emueve una fila cada vez que lo ejecutamos
+#nos devuelve dicha fila en la que estamos posicionados
+
+row = cursor.fetchone()#Primera fila
+print(row)
+row = cursor.fetchone()#segunda fila
+print(row)
+row = cursor.fetchone()#Tercera fila
+print(row)
+row = cursor.fetchone()#Cuarta fila
+print(row)
+#Hasta aquí está la consulta completa de la tabla
+#que sucede si leemos una fila cuando no tenemos más?
+row = cursor.fetchone()#Quinta Fila
+print(row)
+#Da none oNull si fuera oracle
+
+#siempre que finalicemos las acciones, debemos liberar los recursos
+cursor.close()
+connection.close()
+print("Fin de programa")
